082ce022dfe4ea1f93e9aa2647228392df85461e
Bogdan Aleksey <bas@novatek-electro.com>
2022-04-20 22:39:53 +0300

    Script + Setup

--- setup.sh
+++ setup.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 set -e
 
+# Install nginx
 apt-get install -y nginx
 ln -sf /opt/opcb-release/nginx-sites/opcb-ui /etc/nginx/sites-enabled/opcb-ui
 systemctl restart nginx
 
+# Install hostapd
+apt-get install -y hostapd
+
+# Install dnsmasq
+apt-get install -y dnsmasq
+
+# Install opcb service
 ln -sf /opt/opcb-release/services/opcb.service /etc/systemd/system/opcb.service
 systemctl daemon-reload
 systemctl enable opcb.service
 systemctl restart opcb.service
 
+# Install update to the crone
 ln -sf /opt/opcb-release/update.sh /etc/cron.daily/opcb-update.sh
