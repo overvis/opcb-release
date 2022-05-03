d07f5b8597a9a6f45ff6da2a928384a8453f5ce8
Bogdan Aleksey <bas@novatek-electro.com>
2022-05-03 22:57:11 +0300

    Setup fix, after test

--- setup.sh
+++ setup.sh
@@ -9,10 +9,6 @@ apt-get install -y nginx
 ln -sf /opt/opcb-release/nginx-sites/opcb-ui /etc/nginx/sites-enabled/opcb-ui
 systemctl restart nginx
 
-# Install hostapd & dnsmasq
-apt-get install -y hostapd dnsmasq
-systemctl unmask hostapd.service
-
 # Install opcb service
 ln -sf /opt/opcb-release/services/opcb.service /etc/systemd/system/opcb.service
 systemctl daemon-reload
